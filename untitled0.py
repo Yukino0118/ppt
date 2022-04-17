35023b8c89632eb169c1faa6655fd476ebda5384
Yukino0118 <91052421+Yukino0118@users.noreply.github.com>
2022-04-17 19:34:34 +0800

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import re
+import pandas as pd
 
-fig = plt.figure(figsize=(6,2))
-data=[[1,2,3],
-      [5,6,7],
-      [8,9,10]]
-column_labels=["Column 1", "Column 2", "Column 3"]
+df = pd.read_excel('1.xlsx', header = None,index_col=None, sheet_name = '1')
+print(df)
+fig = plt.figure(figsize=(5,3))
+
+column_labels=["B","C"]
 plt.axis('off')
 plt.grid('off')
-tb = plt.table(cellText=data,colLabels=column_labels,cellLoc='center', loc="center")
+tb = plt.table(cellText=df.values,cellLoc='center', loc="center")
 #set font bold and color
 for (row, col), cell in tb.get_celld().items():
     if (row == 0) or (col == 0):
